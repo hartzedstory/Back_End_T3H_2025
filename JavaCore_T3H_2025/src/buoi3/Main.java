1a477ba4ba8edc05ca0b628810daec53b3e3a736
Duc Nguyen <ducnafs1579@gmail.com>
2025-03-04 19:46:18 +0700

    BTVN

--- JavaCore_T3H_2025/src/buoi3/Main.java
+++ JavaCore_T3H_2025/src/buoi3/Main.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
     public static void LectureExercise() {
-        Exercise1();
+        Exercise3();
     }
     public static void Exercise1() {
         System.out.println("Nhap so nguyen duong");
@@ -41,8 +41,55 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int numberOfDigit = 0;
+        int reversed = 0;
+        int digit = 0;
+        while (n != 0) {
+            digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n = n / 10;
+        }
+        System.out.println(reversed);
+    }
 
-        for (int i = 0; i <= n; i++) {}
+    public static void Exercise3() {
+        // Tính giai thừa của số n
+        System.out.println("Nhap so");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum = sum * i;
+        }
+        System.out.println(sum);
+    }
+
+    public static void Exercise4() {
+        int tong = 200000;
+        int menhGia1 = 1000;
+        int menhGia2 = 2000;
+        int menhGia3 = 5000;
+
+        int tongMenhGia1 = tong / menhGia1;
+        int tongMenhGia2 = tong / menhGia2;
+        int tongMenhGia3 = tong / menhGia3;
+
+        for (int i = 0; i <= tongMenhGia1; i++) {
+            int soToMenhGia1 = i;
+            for (int j = 0; j <= tongMenhGia2; j++) {
+                int soToMenhGia2 = j;
+                for (int k = 0; k <= tongMenhGia3; k++) {
+                    int soToMenhGia3 = k;
+
+                    int tongTienMenhGia1 = soToMenhGia1 / menhGia1;
+                    int tongTienMenhGia2 = soToMenhGia2 / menhGia2;
+                    int tongTienMenhGia3 = soToMenhGia3 / menhGia3;
+
+                    if (tong == (tongTienMenhGia1 + tongTienMenhGia2 + tongTienMenhGia3)) {
+                        System.out.println(String.format("So to 1000: %d,So to 2000: %d,So to 3000: %d", soToMenhGia1, soToMenhGia2, soToMenhGia3));
+                    }
+                }
+            }
+        }
     }
 
     public static void Lecture() {
